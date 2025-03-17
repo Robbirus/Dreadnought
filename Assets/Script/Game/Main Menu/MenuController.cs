bacb1bbc1d64ff380cb75f2056daa15d4e591ad4
faivr <faivr@CLEMENT-DESKTOP>
2025-03-17 23:40:29 +0100

    CFA : Ajout de PlayerPref Editor Avancement du Menu, boite son (juste volume, prévoir MasterVolume, volume, SFX)

--- Assets/Script/Game/Main Menu/MenuController.cs
+++ Assets/Script/Game/Main Menu/MenuController.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [SerializeField] private GameObject confirmationPrompt = null;
+
     [Header("Levels to Load")]
     [SerializeField] private string newGameLevel;
     [SerializeField] private GameObject noSavedGameDialog = null;
@@ -32,5 +41,36 @@ public class MainMenu : MonoBehaviour
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void ApplyVolume()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        // Show Prompt
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            ApplyVolume();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confirmationPrompt.SetActive(false);
     }
 }
