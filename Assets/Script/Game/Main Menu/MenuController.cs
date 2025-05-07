8cc2c5affd3b5998d12ff93d12b167ef410ec48f
faivr <faivr@LAPTOP-UUJEBSBS>
2025-05-08 01:44:31 +0200

    🐛 : Fix d'un bug sur le son dans le menu

--- Assets/Script/Game/Main Menu/MenuController.cs
+++ Assets/Script/Game/Main Menu/MenuController.cs
@@ -240,7 +240,7 @@ public class MenuController : MonoBehaviour
         {
             sfxVolume++;
         }
-        audioMixer.SetFloat("BGM", Mathf.Log10(sfxVolume / 10f) * 20);
+        audioMixer.SetFloat("SFX", Mathf.Log10(sfxVolume / 10f) * 20);
         sfxTextValue.text = sfxVolume.ToString("0");
     }
 
@@ -253,11 +253,11 @@ public class MenuController : MonoBehaviour
 
         if (sfxVolume == 0)
         {
-            audioMixer.SetFloat("BGM", Mathf.Log10(-1 * 20));
+            audioMixer.SetFloat("SFX", Mathf.Log10(-1 * 20));
         }
         else
         {
-            audioMixer.SetFloat("BGM", Mathf.Log10(sfxVolume / 10f) * 20);
+            audioMixer.SetFloat("SFX", Mathf.Log10(sfxVolume / 10f) * 20);
         }
 
          sfxTextValue.text = sfxVolume.ToString("0");
