0209a2483d4d34ac53922c8aed87697238e46c82
faivr <faivr@CLEMENT-DESKTOP>
2026-02-06 01:30:01 +0100

    :lipstick: Change sounds, It's bakc in 2D

--- Assets/Script/Collectable/CollectableSoundManager.cs
+++ Assets/Script/Collectable/CollectableSoundManager.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class CollectableSoundManager : MonoBehaviour
 {
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource collectableSource;
+
+    private void Awake()
+    {
+        if (collectableSource == null)
+            collectableSource = GetComponent<AudioSource>();
+    }
+
     public void Play(AudioClip clip)
     {
-        AudioSource.PlayClipAtPoint(clip, transform.position);
+        GameObject temp = new GameObject("CollectSound");
+        collectableSource = temp.AddComponent<AudioSource>();
+        collectableSource.spatialBlend = 0f;
+        collectableSource.volume = 0.6f;
+        collectableSource.PlayOneShot(clip);
+        Destroy(temp, clip.length);
     }
 }
